cc7b289c2a99a4ee172f4c7eb94a14e0a20c005c
erinsawyer504 <erinsawyer504@gmail.com>
2022-12-10 17:56:57 -0500

    Added functions to randomnize the numbers/letters/symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,73 @@
 // Assignment code here
-let numsOnly = "0123456789";
-let lcOnly= "abcdefghijklmnopqrstuvwxyz";
-let ucOnly= "ABCDEFGHIJKLMNOPQRSTUVWYZY";
-let symOnly= '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+let nums = "0123456789";
+let lower= "abcdefghijklmnopqrstuvwxyz";
+let upper= "ABCDEFGHIJKLMNOPQRSTUVWYZY";
+let symbols= '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
 let genpassword = "";
 let useLC = false;
 let useUC = false;
 let useNum = false;
 let useSym = false;
 
-console.log(symOnly);
+
 //setting up prompts for user to answer
-let pwLength = prompt ("How long do you want your password to be?, Enter between 8 & 128 characters"); 
-let pwLC = prompt ("Do you want to include lowercase letters?");
-let pwUC = prompt ("Do you want to include uppercase letters?");
-let pwNum = prompt ("Do you want to include numeric characters?");
-let pwSym = prompt ("Do you want to include special characters?");
+let pwLength = window.prompt ("How long do you want your password to be?, Enter between 8 & 128 characters"); 
+let askLC = prompt ("Do you want to include lowercase letters?");
+let askUC = prompt ("Do you want to include uppercase letters?");
+let askNum = prompt ("Do you want to include numeric characters?");
+let askSym = prompt ("Do you want to include special characters?");
+
+//Ends function if user presses cancel
+/*if (pwLength === null) {
+  return;
+};*/
 
 //turns the pwLength from a string to a number
 let numPwLength = parseInt(pwLength);
 
 //setting answers to prompts to all lower case
-pwLC = pwLC.toLowerCase();
-pwUC = pwUC.toLowerCase();
-pwNum = pwNum.toLowerCase();
-pwSym = pwSym.toLowerCase();
+askLC = askLC.toLowerCase();
+askUC = askUC.toLowerCase();
+askNum = askNum.toLowerCase();
+askSym = askSym.toLowerCase();
 
 //setting the answers to the prompt to boolean values
-if (pwLC === "y" || pwLC === "yes") {
+if (askLC === "y" || askLC === "yes") {
   useLC = true;
 };
 
-if (pwUC === "y" || pwUC === "yes") {
+if (askUC === "y" || askUC === "yes") {
   useUC = true;
 };
 
-if (pwNum === "y" || pwNum === "yes") {
+if (askNum === "y" || askNum === "yes") {
   useNum = true;
 };
 
-if (pwSym === "y" || pwSym === "yes") {
+if (askSym === "y" || askSym === "yes") {
   useSym = true;
 };
 
-
-
-
-if (numPwLength >7 && numPwLength <129) {
-  for (var i=0; i <= numPwLength; i++) {
-    var randomNumber = Math.floor(Math.random() * numsOnly);
-    password += numsOnly.substring(randomNumber, randomNumber +1);
-  };
-  console.log(randomNumber);
-
-
-} else {
-  
-  console.log ("Please enter a number between 8 and 128");
-  
+//Setting up functions
+function genRandomLC () {
+  return lower[Math.floor(Math.random() * lower.length)];
 }
+
+function genRandomUC () {
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+
+function genRandomNumber() {
+  return nums[Math.floor(Math.random() * nums.length)];
+}
+
+function genRandomSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(genRandomLC());
+console.log(genRandomUC());
+console.log(genRandomNumber());
+console.log(genRandomSymbol());
 
 
 // Get references to the #generate element
